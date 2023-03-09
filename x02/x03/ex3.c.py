be048c2f943c9ad12af9990955a5d553e59ded8e
Will Bettridge-Radford <william.bettridge.radford@gmail.com>
2023-03-09 08:05:35 +0000

    ex3

---
+++ x02/x03/ex3.c.py
@@ -0,0 +1,12 @@
+include_lib("stdio.h")
+
+def main() -> int:
+    age: int = 10
+    height: int = 72
+
+
+    printf("I am %d years old.\n", age)
+    printf("I am %d inches tall.\n", height)
+
+    return 0
+
